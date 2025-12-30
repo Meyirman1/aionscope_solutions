db9e13487dff384154501b92a703bae8969cea1e
Meyirman1 <147164354+Meyirman1@users.noreply.github.com.>
2025-12-30 08:33:24 +0500

    Added watchdog that automatically runs the script the moment a new file is added to report_samples dir

--- AS_archiver/vv1/archiver.py
+++ AS_archiver/vv1/archiver.py
@@ -6,6 +6,13 @@
 
 import json
 from pathlib import Path 
+import pydicom
+from datetime import datetime
+# import logging ##pip install
+# import sentry ##pip install
+import time
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 
 
 src_dir = Path('report_samples')
@@ -28,6 +35,7 @@ for each_dir in all_dirs:
     each_dir.mkdir(parents=True, exist_ok=True)
 
 src_reports = list(src_dir.glob('*.txt'))
+src_dcm_reports = list(src_dir.glob('*.dcm'))
 
 def extract_metadata(patient_id,date,patient_fname,patient_lname,modality):
     extract  = {
@@ -39,7 +47,7 @@ def extract_metadata(patient_id,date,patient_fname,patient_lname,modality):
                  }
     return extract
 
-def archiving_report(orig_reports):
+def arch_reports(orig_reports):
     try:
         for orig_report in orig_reports:
             parts = orig_report.stem.split('_')
@@ -68,7 +76,7 @@ def archiving_report(orig_reports):
             report_text = orig_report.read_text()
             dest_report_fp.write_text(report_text)
             lv_dest_report_fp.write_text(report_text)
-            print(f"\nThe report with ID:{parts[1]} was archived successfully!")
+            print(f"The report with ID:{parts[1]} was archived successfully!")
                     
             with open(dest_meta_fp, 'w') as json_ouput:
                 json_dest = json_ouput
@@ -79,4 +87,128 @@ def archiving_report(orig_reports):
     except FileNotFoundError:
         print("\tERROR: Check if the targeted filepaths are correct.".upper())
     
-archiving_report(src_reports)
+def extract_dcm_header(dcm_report):
+    get_id = dcm_report.get('PatientID','UnknownPatientID')
+    get_date = dcm_report.get('ContentDate') or dcm_report.get('AcquisitionDate') or dcm_report.get('StudyDate') or dcm_report.get('InstanceCreationDate')
+    get_name = dcm_report.get('PatientName','UnknownPatientName')
+    get_sex = dcm_report.get('PatientSex','UnknownPatientSex')
+    get_si_uid = dcm_report.get('StudyInstanceUID','UnknownStudyInstanceUID')
+    get_modality = dcm_report.get('Modality','UnknownModality')
+    
+    if get_date and str(get_date).isdigit():
+        try:
+            format_date = datetime.strptime(get_date,"%Y%m%d").strftime("%d-%m-%Y")
+        except(TypeError,ValueError):
+            format_date = 'Corrupted Date'
+    else:
+        format_date = 'UnknownDate'
+
+    
+
+    format_name = str(get_name).replace("^"," ")
+
+    extract  = {
+        "patient_id": f"P{get_id}",
+        "date": f"{format_date}",
+        "patient_name": f"{format_name}",
+        "patient_gender": f"{get_sex}",
+        "study Instance UID": f"{get_si_uid}",
+        "modality": f"{get_modality}"
+                 }
+    return extract
+
+def arch_dcm_reports(dcm_reports):
+    try:
+        for dcm_report in dcm_reports:
+            header_json = dcm_report.with_suffix('.json')
+
+            lv_dest_report_fp = lv_reports/dcm_report.name
+            lv_dest_meta_fp = lv_meta/header_json.name
+
+            dest_report_fp = arch_reports_dir / dcm_report.name
+            dest_meta_fp = meta_dir/header_json.name
+
+            ds = pydicom.dcmread(dcm_report)
+            ds.save_as(dest_report_fp)
+            ds.save_as(lv_dest_report_fp)
+            cleaned_dcm = dcm_report.with_suffix('').name
+            print(f"The report {cleaned_dcm} with ID:{ds.PatientID} was archived successfully!")
+
+            payload_header = extract_dcm_header(ds)
+
+            with open(dest_meta_fp, "w") as f_obj:
+                dcm_header = f_obj
+                json.dump(payload_header, dcm_header, indent=4)
+            with open(lv_dest_meta_fp, "w") as f_obj:
+                lv_dcm_header = f_obj
+                json.dump(payload_header, lv_dcm_header, indent=4)
+                
+    except FileNotFoundError:
+        print("\tERROR: Check if the targeted filepaths are correct.".upper())
+
+#also if one dcm report is invalid the script mist keep running for new reports.Do that after watchdog.
+
+def safe_archive(file_path):
+    retries = 5
+    while retries > 0:
+        try:
+            if file_path.suffix.lower() == '.txt':
+                arch_reports([file_path])
+            elif file_path.suffix.lower() == '.dcm':
+                arch_dcm_reports([file_path])
+            return
+        except PermissionError:
+            print(f"   [!] File {file_path.name} is busy. Retrying in 1s... ({retries} left)")
+            time.sleep(1)
+            retries -= 1
+        except Exception as e:
+            print(f"   [!] Unexpected error: {e}")
+            break
+    print(f"   [X] FAILED to archive {file_path.name} after multiple attempts.")
+
+class ReportHandler(FileSystemEventHandler):
+   def on_created(self, event):
+    ##Checking if event is just a folder if True it ignores it with 'return'
+    if event.is_directory:
+        return
+    try:   
+        raw_path = str(event.src_path)
+        file_path = Path(raw_path).resolve()
+        ## get the actual name of the file is just created
+        print(f"\nDetected new file and is being processed: {file_path.name}") 
+        
+
+    except Exception as e:
+        print(f"[!] ERROR processing {str(event.src_path)}: {e}")
+
+    ##Observer creation is the guard
+observer = Observer()
+handler = ReportHandler()
+
+##Giving the guard the manual(handler)
+observer.schedule(handler, path=str(src_dir), recursive=False)
+##start the guard shift
+observer.start()
+print("Watching the folder:")
+print("-" * 20)
+try:
+    while True:
+        time.sleep(0.5)
+        # The script "sleeps" for 1 second, then loops again.
+    # This uses almost zero CPU power.
+except KeyboardInterrupt:
+    observer.stop()
+
+# Wait for the guard to finish packing up before closing completely.
+observer.join()
+
+
+
+
+
+    
+       
+          
+
+
+
